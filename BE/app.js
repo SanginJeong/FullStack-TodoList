9a05545de1c93f79ccbab4bf9eb1a28c3dfd86e0
SangIn Jeong <sanginjeong07@gmail.com>
2024-02-21 03:39:01 +0900

    Update : FE complete

--- BE/app.js
+++ BE/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
+const cors = require('cors');
 // 정의한 라우터 갖고오기
 const indexRouter = require('./routes/index');
 
@@ -9,11 +9,11 @@ const app = express();
 
 // bodyParser 를 사용하여 객체형태로 불러옴
 app.use(bodyParser.json());
+app.use(cors());
 
 // /api라는 주소로 가면 indexRouter를 사용한다
 // /api => index => /tasks => 라우팅  구조
 app.use('/api',indexRouter)
-
 const mongoURI = 'mongodb://localhost:27017/todo-demo';
 
 // {useNewUrlParser:true} : 최신 형식도 사용할 수 있도록
